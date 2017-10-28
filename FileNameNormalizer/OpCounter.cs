b1846801f6fe48fae320434a2fd48701591ca8b6
khaapamaki <kati.haapamaki@gmail.com>
2017-10-28 17:37:14 +0300

    Non-plural post-op report messages

--- FileNameNormalizer/OpCounter.cs
+++ FileNameNormalizer/OpCounter.cs
@@ -31,7 +31,6 @@ namespace FileNameNormalizer
         public int FilesWithSpacesProducedDuplicate { get; set; } = 0;
         public int DirsWithSpacesProducedDuplicate { get; set; } = 0;
 
-
         public int FilesWithDuplicateNamesCreated { get; set; } = 0;
         public int DirsWithDuplicateNamesCreated { get; set; } = 0;
 
@@ -42,78 +41,145 @@ namespace FileNameNormalizer
             StringBuilder s = new StringBuilder(1000);
 
             if (FilesNeedNormalize > 0) {
-                s.AppendLine($"{FilesNeedNormalize} files need normalization");
+                if (IsPlural(FilesNeedNormalize))
+                    s.AppendLine($"{FilesNeedNormalize} filenames need normalization");
+                else
+                    s.AppendLine($"1 filename needs normalization");
 
                 if (FilesNeedNormalizeRenamed > 0 || FilesNeedNormalizeFailed > 0) {
                     if (FilesNeedNormalizeRenamed > 0) {
-                        s.AppendLine($"   of which {FilesNeedNormalizeRenamed} files were normalized");
+                        if (IsPlural(FilesNeedNormalizeRenamed))
+                            s.AppendLine($"   of which {FilesNeedNormalizeRenamed} filenames were normalized succesfully");
+                        else
+                            s.AppendLine("    of which 1 filename was normalized succesfully");
                     }
                     if (FilesNeedNormalizeFailed > 0) {
-                        s.AppendLine($"   with {FilesNeedNormalizeFailed} files failed to be normalized");
+                        if (IsPlural(FilesNeedNormalizeFailed))
+                            s.AppendLine($"   with {FilesNeedNormalizeFailed} filenames failed to be normalized succesfully");
+                        else
+                            s.AppendLine($"   with 1 filename failed to be normalized succesfully");
                     }
                     if (FilesNeedNormalizeProducedDuplicate > 0) {
-                        s.AppendLine($"   {FilesNeedNormalizeProducedDuplicate} files were renamed with 'Duplicate' tag");
+                        if (IsPlural(FilesNeedNormalizeProducedDuplicate))
+                            s.AppendLine($"   {FilesNeedNormalizeProducedDuplicate} files were renamed with 'Duplicate' tags");
+                        else
+                            s.AppendLine($"   1 file was renamed with a 'Duplicate' tag");
                     }
                 } else {
                     if (FilesNeedNormalizeProducedDuplicate > 0) {
-                        s.AppendLine($"   of which {FilesNeedNormalizeProducedDuplicate} files have duplicate names and need to be renamed");
+                        if (IsPlural(FilesNeedNormalizeProducedDuplicate))
+                            s.AppendLine($"   of which {FilesNeedNormalizeProducedDuplicate} files have duplicate names and need to be renamed");
+                        else
+                            s.AppendLine($"   of which 1 file has a duplicate name and needs to be renamed");
                     }
                 }
             }
 
             if (DirsNeedNormalize > 0) {
-                s.AppendLine($"{DirsNeedNormalize} folders need normalization");
+                if (IsPlural(DirsNeedNormalize))
+                    s.AppendLine($"{DirsNeedNormalize} foldernames need normalization");
+                else
+                    s.AppendLine($"1 foldername needs normalization");
 
                 if (DirsNeedNormalizeRenamed > 0 || DirsNeedNormalizeFailed > 0) {
                     if (DirsNeedNormalizeRenamed > 0) {
-                        s.AppendLine($"   of which {DirsNeedNormalizeRenamed} folders were normalized");
+                        if (IsPlural(DirsNeedNormalizeRenamed))
+                            s.AppendLine($"   of which {DirsNeedNormalizeRenamed} foldernames were normalized succesfully");
+                        else
+                            s.AppendLine($"   of which 1 foldername was normalized succesfully");
                     }
                     if (DirsNeedNormalizeFailed > 0) {
-                        s.AppendLine($"   with {DirsNeedNormalizeFailed} folders failed to be normalized");
+                        if (IsPlural(DirsNeedNormalizeFailed))
+                            s.AppendLine($"   with {DirsNeedNormalizeFailed} foldernames failed to be normalized");
+                        else
+                            s.AppendLine($"   with 1 foldername failed to be normalized");
                     }
                     if (DirsNeedNormalizeProducedDuplicate > 0) {
-                        s.AppendLine($"   {DirsNeedNormalizeProducedDuplicate} folders were renamed with 'Duplicate' tag");
+                        if (IsPlural(DirsNeedNormalizeProducedDuplicate))
+                            s.AppendLine($"   {DirsNeedNormalizeProducedDuplicate} folders were renamed with 'Duplicate' tags");
+                        else
+                            s.AppendLine($"   1 folder was renamed with a 'Duplicate' tag");
                     }
                 } else {
                     if (DirsNeedNormalizeProducedDuplicate > 0) {
-                        s.AppendLine($"   of which {DirsNeedNormalizeProducedDuplicate} folders have duplicate names and need to be renamed");
+                        if (IsPlural(DirsNeedNormalizeProducedDuplicate))
+                            s.AppendLine($"   of which {DirsNeedNormalizeProducedDuplicate} folders have duplicate names and need to be renamed");
+                        else
+                            s.AppendLine($"   of which 1 folder has a duplicate name and needs to be renamed");
                     }
                 }
             }
 
             if (FilesWithDuplicateNames > 0) {
-                s.AppendLine($"{FilesWithDuplicateNames} files have a duplicate name in case sensitive domain");
-                if (FilesWithDuplicateNamesRenamed > 0)
-                    s.AppendLine($"   of which {FilesWithDuplicateNamesRenamed} files were renamed with 'Duplicate' tag");
-                if (FilesWithDuplicateNamesFailed > 0)
-                    s.AppendLine($"   with {FilesWithDuplicateNamesFailed} failed to be renamed");
+                if (IsPlural(FilesWithDuplicateNames))
+                    s.AppendLine($"{FilesWithDuplicateNames} files have duplicate names in case sensitive domain");
+                else
+                    s.AppendLine($"1 file has a duplicate name in case sensitive domain");
+
+                if (FilesWithDuplicateNamesRenamed > 0) {
+                    if (IsPlural(FilesWithDuplicateNamesRenamed))
+                        s.AppendLine($"   of which {FilesWithDuplicateNamesRenamed} files were renamed with 'Duplicate' tags");
+                    else
+                        s.AppendLine($"   of which 1 file was renamed with a 'Duplicate' tag");
+                }
+                if (FilesWithDuplicateNamesFailed > 0) {
+                    if (IsPlural(FilesWithDuplicateNamesFailed))
+                        s.AppendLine($"   with {FilesWithDuplicateNamesFailed} files failed to be renamed");
+                    else
+                        s.AppendLine($"   with 1 file failed to be renamed");
+                }
+
             }
             if (DirsWithDuplicateNames > 0) {
-                s.AppendLine($"{DirsWithDuplicateNames} folders have a duplicate name in case sensitive domain");
-                if (DirsWithDuplicateNamesRenamed > 0)
-                    s.AppendLine($"   of which {DirsWithDuplicateNamesRenamed} folders were renamed with 'Duplicate' tag");
-                if (DirsWithDuplicateNamesFailed > 0)
-                    s.AppendLine($"   with {DirsWithDuplicateNamesFailed} failed to be renamed");
+                if (IsPlural(DirsWithDuplicateNames))
+                    s.AppendLine($"{DirsWithDuplicateNames} folders have duplicate names in case sensitive domain");
+                else
+                    s.AppendLine($"1 folder has a duplicate name in case sensitive domain");
+                if (DirsWithDuplicateNamesRenamed > 0) {
+                    if (IsPlural(DirsWithDuplicateNamesRenamed))
+                        s.AppendLine($"   of which {DirsWithDuplicateNamesRenamed} folders were renamed with 'Duplicate' tags");
+                    else
+                        s.AppendLine($"   of which 1 folder was renamed with a 'Duplicate' tag");
+                }
+                if (DirsWithDuplicateNamesFailed > 0) {
+                    if (IsPlural(DirsWithDuplicateNamesFailed))
+                        s.AppendLine($"   with {DirsWithDuplicateNamesFailed} failed to be renamed");
+                    else
+                        s.AppendLine($"   with 1 folder failed to be renamed");
+                }
+
             }
 
             if (FilesWithSpaces > 0) {
-                s.AppendLine($"{FilesWithSpaces} files that has leading or trailing spaces. Not fixed");
+                if (IsPlural(FilesWithSpaces))
+                    s.AppendLine($"{FilesWithSpaces} filenames that have leading or trailing spaces. Not fixed");
+                else
+                    s.AppendLine($"1 filename that has leading or trailing spaces. Not fixed");
             }
             if (DirsWithSpaces > 0) {
-                s.AppendLine($"{DirsWithSpaces} files that has leading or trailing spaces. Not fixed");
+                if (IsPlural(DirsWithSpaces))
+                    s.AppendLine($"{DirsWithSpaces} foldernames that have leading or trailing spaces. Not fixed");
+                else
+                    s.AppendLine($"1 foldername that has leading or trailing spaces. Not fixed");
             }
 
             if (TooLongPaths > 0) {
-                s.AppendLine($"{TooLongPaths} files or folders that has too long path. Not fixed");
+                if (IsPlural(TooLongPaths))
+                    s.AppendLine($"{TooLongPaths} files or folders have too long path. Not fixed");
+                else
+                    s.AppendLine($"{TooLongPaths} file or folder has too long path. Not fixed");
             }
 
             if (s.ToString() == "") {
-                s.AppendLine("Found Nothing.");
+                s.AppendLine("Found nothing. Going home.");
             }
 
             return s.ToString();
         }
-
+        private bool IsPlural(int number)
+        {
+            return number == 1 ? false : true;
+        }
     }
 
 
